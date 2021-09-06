a676e67c1482e9db5aec11e6e061b426a17729d6
muhammad faris <rizkymuhammad.farisprakoso@mitrais.com>
2021-09-06 08:12:52 +0700

    feat: register auth reducer to combinator

---
+++ src/store/reducers/index.tsx
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import { authReducer } from "./auth.reducer";
+
+const reducers = combineReducers({
+    auth: authReducer
+})
+
+export type RootState = ReturnType<typeof reducers>
+
+export default reducers
